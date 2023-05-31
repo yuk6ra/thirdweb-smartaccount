7ee9792e987dd979a2905777bb84a973d257bb0a
yuk6ra <yuki.imamura.all@gmail.com>
2023-05-31 18:39:22 +0900

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,28 @@
 import type { AppProps } from "next/app";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ThirdwebProvider, localWallet, metamaskWallet, smartWallet } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+const activeChain = "mumbai";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={activeChain}>
+    <ThirdwebProvider 
+        activeChain={activeChain}
+        supportedWallets={[
+            smartWallet({
+                factoryAddress: "0x640b17bD959c3528aF1dB7a1E70233e8E7E5327F",
+                thirdwebApiKey: "7527e7b843c07b73a1de496d8cac15ad476f1f1049a3c3017c7e3c00f44fb2a45b0f0e549841eefe03003c7e38d6ca3f36e0dab700b987f3b1513161e8ebdfc5",
+                gasless: true,
+                personalWallets: [
+                    metamaskWallet(),
+                    localWallet()
+                ]
+            })
+        ]}    
+    >
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,101 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import {
+    ConnectWallet,
+    Web3Button,
+    useContract,
+    useContractRead,
+    useContractWrite,
+    useAddress,
+    ThirdwebSDK
+} from "@thirdweb-dev/react";
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
+import { useEffect, useState } from "react";
 
 const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="http://thirdweb.com/">thirdweb</a>!
-        </h1>
 
-        <p className={styles.description}>
-          Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.tsx</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.tsx</code> file!
-        </p>
+    const [ isSuccess, setIsSuccess ] = useState<boolean>(false);
+    const [ txnHash, setTxnHash ] = useState<string>("");
 
-        <div className={styles.connect}>
-          <ConnectWallet />
+    const address = useAddress();
+    const walletAddress = 
+    console.log("address", address)
+
+    const { contract } = useContract("0xDA4dc54472573B0193394d6214DCD767Bd295241");
+    const { mutateAsync: mintTo, isLoading } = useContractWrite(contract, "mintTo")
+
+    // const { contract } = useContract("0x640b17bD959c3528aF1dB7a1E70233e8E7E5327F");
+    // const { data, isLoading } = useContractRead(contract, "getAddress", [address])
+
+    const call = async () => {
+        try {
+            const data = await mintTo({ args: [address, "_uri"] });
+            console.info("contract call successs", data);
+            setTxnHash(data.receipt.transactionHash);
+            setIsSuccess(true);
+        } catch (err) {
+            console.error("contract call failure", err);
+            setIsSuccess(false)
+        }
+    }
+
+    return (
+        <div className={styles.container}>
+            <main className={styles.main}>
+
+                <h1 className={styles.title}>
+                    AA Wallet
+                </h1>
+
+                <h2 className={styles.subtitle}>
+                    Step 1. Confirm your Smart Account
+                </h2>
+
+                <text className={styles.text}>
+                    {address && (
+                        <a href={`https://mumbai.polygonscan.com/address/${address}`}>
+                            {address}
+                       </a> 
+                    )}
+                    {!address && <ConnectWallet />}
+                </text>
+
+                <a href="" ></a>
+
+                <h2 className={styles.subtitle}>
+                    Step 2. Claim your AA Tokens
+                </h2>
+                        
+                {address && (
+                    <Web3Button
+                        contractAddress={"0xDA4dc54472573B0193394d6214DCD767Bd295241"}
+                        action={() => call()}
+                    >
+                        Get AA Tokens
+                    </Web3Button>
+                )}
+                <text
+                    className={styles.text}
+                >
+                    {isSuccess && txnHash}
+                </text>
+
+                <h2 className={styles.subtitle}>
+                    Step 3. Check Transaction
+                </h2>
+                
+                {isSuccess && (
+                    <button 
+                        className={styles.button}
+                        onClick={()=> window.open(`https://mumbai.polygonscan.com/tx/${txnHash}`)}
+                    >
+                            Check Transaction            
+                    </button>
+                )}
+
+                {!isSuccess && <text className={styles.text}>Please claim your AA Tokens</text>}
+            </main>
         </div>
-
-        <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a
-            href="https://portal.thirdweb.com/templates"
-            className={styles.card}
-          >
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+    );
 };
 
 export default Home;
